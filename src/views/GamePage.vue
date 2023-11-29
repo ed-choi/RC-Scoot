a507ea541542b3c150c19a727f9716dd5db3eb35
Timothy Yong <tyong_23@hotmail.com>
2023-11-29 13:15:01 -0800

    cube

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -13,24 +13,39 @@
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, render } from 'vue';
 import * as THREE from 'three';
 
+const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75, window.innerWidth / window.innerHeight, 0.1, 1000);
-scene.add(camera);
 
-let renderer;
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00d000 });
+const cube = new THREE.Mesh(geometry, material);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
 
 onMounted(() => {
-  let scenebox = document.getElementById("scenebox");
-  let canvasElement = scenebox as unknown as HTMLDivElement;
-  renderer = new THREE.WebGLRenderer();
+  const scenebox = document.getElementById("scenebox");
+  const canvasElement = scenebox as unknown as HTMLDivElement;
+
+  // from tutorial
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(1.0);
-  canvasElement.append(renderer.domElement);
-  renderer.render(scene, camera);
+  canvasElement.appendChild(renderer.domElement);
+
+  camera.position.set(2, 2, 2);
+  camera.lookAt(0, 0, 0);
+
+  scene.add(cube);
+
+  animate();
 });
 
 
